3723d117387ada3c470f9fbe59e311520fd373ef
Gyanu Sahu <gyanuraj23330@gmail.com>
2024-05-23 17:51:01 +0530

    Final Commit

---
+++ react-booking-ui/src/components/featured/Featured.jsx
@@ -0,0 +1,44 @@
+import "./featured.css";
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1614082242765-7c98ca0f3df3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>GOA</h1>
+          <h2>123 properties</h2>
+        </div>
+      </div>
+
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1598091383021-15ddea10925d?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Kashmir</h1>
+          <h2>533 properties</h2>
+        </div>
+      </div>
+      <div className="featuredItem">
+        <img
+          src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+          className="featuredImg"
+        />
+        <div className="featuredTitles">
+          <h1>Kerala</h1>
+          <h2>532 properties</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
